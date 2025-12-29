2f7d2b55815f5d59accf296b192f4a437a695f18
Zack Brown <zackaryzbrown@gmail.com>
2025-12-28 22:42:56 -0700

    Footer big fix

--- src/App.js
+++ src/App.js
@@ -19,27 +19,29 @@ function App() {
   const location = useLocation();
   const isFalse = location.pathname.includes("404");
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <ScrollToTop />
       {isFalse || <Navbar />}
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/project/:id" element={<ProjectDetails />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/404" element={<NotFound />} />
-          <Route path="*" element={<Navigate replace to="/404" />}></Route>
-          <Route path="/mern-blog" element={<MernBlogRepair />} />
-          <Route
-            path="/employee-salary-management"
-            element={<EmployeeSalaryManagementRepair />}
-          />
-        </Routes>
-      </Suspense>
+      <main className="flex-1">
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/project/:id" element={<ProjectDetails />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<Navigate replace to="/404" />}></Route>
+            <Route path="/mern-blog" element={<MernBlogRepair />} />
+            <Route
+              path="/employee-salary-management"
+              element={<EmployeeSalaryManagementRepair />}
+            />
+          </Routes>
+        </Suspense>
+      </main>
       {isFalse || <Footer />}
-    </>
+    </div>
   );
 }
 

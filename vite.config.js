b367732387894bf54c714d13ff4e1cd905f1c163
Zack Brown <zackaryzbrown@gmail.com>
2026-03-04 00:37:49 -0700

    .env example / congfig changes

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    include: /src\/.*\.js$/,
+    exclude: [],
+    loader: "jsx",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/setupTests.js",
+  },
+});

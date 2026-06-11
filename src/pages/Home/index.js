0e632555b0c1ab5ef10c0f8ba4edb15a94f6afac
Zack Brown <zackaryzbrown@gmail.com>
2026-06-11 16:17:23 -0600

    Refactor Home component for improved layout; update class names for better styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import Project from "../Project";
 import Contact from "../Contact";
 import { Banner, ExperienceSnapshot, Testimonial } from "../../components";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="pt-16">
+    <div className="home-page pt-20">
       <Banner />
       <ExperienceSnapshot />
       <Project />

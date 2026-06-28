db0bb6dbd3184edf67f82c8c16a5b32986b5936f
Zack Brown <zackaryzbrown@gmail.com>
2026-06-28 16:20:53 -0600

    Refactor index.js for improved export structure; streamline component exports for better readability

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,25 +1,9 @@
-import Banner from "./Banner";
-import Education from "./Education";
-import ExperienceSnapshot from "./ExperienceSnapshot";
-import Footer from "./Footer";
-import HeroUtilityBar from "./HeroUtilityBar";
-import MySkill from "./MySkill";
-import Navbar from "./Navbar";
-import NotFound from "./NotFound";
-import ProjectDetails from "./ProjectDetails";
-import Service from "./Service";
-import Testimonial from "./Testimonial";
-
-export {
-  Banner,
-  Education,
-  ExperienceSnapshot,
-  Footer,
-  HeroUtilityBar,
-  MySkill,
-  Navbar,
-  NotFound,
-  ProjectDetails,
-  Service,
-  Testimonial,
-};
+export { default as Banner } from "./Banner";
+export { default as ExperienceSnapshot } from "./ExperienceSnapshot";
+export { default as Footer } from "./Footer";
+export { default as HeroUtilityBar } from "./HeroUtilityBar";
+export { default as Navbar } from "./Navbar";
+export { default as NotFound } from "./NotFound";
+export { default as ProjectDetails } from "./ProjectDetails";
+export { default as Service } from "./Service";
+export { default as Testimonial } from "./Testimonial";
